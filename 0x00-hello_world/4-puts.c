aa50eb32efa70c0348250dbcea4cbe5925aedd9a
rencyngina <rencyngina@gmail.com>
2022-06-03 06:26:02 -0700

    man de die

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
- * main - prints the phrase "programming is like building a multiling puzzle
+ * main - prints the phrase "Programming is like building a multilingual puzzle
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
