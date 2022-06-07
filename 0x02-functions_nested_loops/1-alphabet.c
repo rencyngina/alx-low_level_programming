21fd6cd7c572995f773f98b1845b055b07e077e3
rencyngina <rencyngina@gmail.com>
2022-06-07 16:29:42 -0700

    lord help

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,13 +7,13 @@ void print_alphabet(void)
 {
 		char ch;
 
-			ch = 'a';
+		ch = 'a';
 
-				while (ch <= 'z')
-						{
-									_putchar(ch);
-											ch++;
-												}
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
 
-					_putchar('\n');
+		_putchar('\n');
 }
