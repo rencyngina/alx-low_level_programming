e588c530989f9f07b464b30db71a5b1e87c75118
rencyngina <rencyngina@gmail.com>
2022-06-06 17:19:02 -0700

    my lord

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
  * main - prints the size of various types
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	print("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
