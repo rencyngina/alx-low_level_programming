f45ab1658146695adbc62272d604f0f0ef261a5e
rencyngina <rencyngina@gmail.com>
2022-06-29 16:02:01 -0700

    kakeguri

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,40 +17,40 @@ int _atoi(char *s)
 	len = 0;
 	f = 0;
 	digit = 0;
-	
-	while (s[len] != '\0')	
-		len++;	
+
+	while (s[len] != '\0')
+		len++;
 
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
-		
-		if (s[i] >= '0' && s[i] <= '9')	
-		{														
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
 			digit = s[i] - '0';
 			if (d % 2)
-				digit = -it;
+				digit = -digit;
 			n = n * 10 + digit;
-			f =1;
+			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
-		{
+		}
 		i++;
 	}
-	
+
 	if (f == 0)
 		return (0);
-			
+
 	return (n);
 }
 
 /**
  * main - adds two positive number
  * @argc: number of arguments
- * @argv: array of arguents
- *
+ * @argv: array of arguments
+ * 
  * Return: 0 (Success), or 1 (Success)
  */
 int main(int argc, char *argv[])
@@ -67,19 +67,19 @@ int main(int argc, char *argv[])
 			{
 				puts("Error");
 				return (1);
-			}			
+			}
 		}
 	}
-	
+
 	for (k = 1; k < argc; k++)
 	{
 		num = _atoi(argv[k]);
-		if (num >= 0)		
+		if (num >= 0)
 		{
 			sum += num;
 		}
 	}
-	
+
 	printf("%d\n", sum);
 	return (0);
 }
